61b9e8d62a12d32829f21aba6f2f9260ce8d7c70
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-03-25 10:30:02 +0500

    init commit

---
+++ app/src/main/java/com/example/lab2ex3coffiebill/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab2ex3coffiebill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
